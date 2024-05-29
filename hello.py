9e9a8286d0362f45af40bcf075d8136286f315fb
Tomás McGrath <54109449+TomasMcG@users.noreply.github.com>
2024-05-29 23:32:06 +0100

    revise data types and functions

--- hello.py
+++ hello.py
@@ -1,2 +1,71 @@
 greeting = 'Hello World!'
 print(greeting)
+
+def my_function():
+      print("Hello from a function") 
+
+my_function()
+
+#Python uses indents for its functions
+
+def fun_input(input):
+      print(input)
+
+fun_input(2)
+
+def fun_input2(input):
+      print(input+2)
+
+fun_input2(2)
+
+#Below are the basic operators
+
+def add(a,b):
+      return a + b
+
+print(add(1,2))
+
+
+def minus(a,b):
+      return a - b
+
+print(minus(10,2))
+
+
+def multiply(a,b):
+      return a * b
+
+print(multiply(5,10))
+
+
+def divide(a,b):
+      return a/b
+
+print(divide(100,10))
+
+#variables
+#int
+x = 10
+y = 20
+z = x + y
+print(z)
+
+#float
+x1=10.0
+y1 = 20
+z1 = x1 + y1
+print(z1)
+#result of a float and int is a float
+
+import math
+float = 12.56
+float2 = 12.56
+math.floor(float)
+math.ceil(float2)
+print(float)
+
+newString = "123"
+string2 = "153"
+print(newString + string2)
+#prints after each other
+
